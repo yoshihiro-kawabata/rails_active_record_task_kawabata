983398aa35304af6d0b8fa86b28fc08fdf58e8f7
yoshihiro_kawabata <yoshihiro_kawabata@mediado.jp>
2022-04-13 22:44:30 +0900

    ActiveRecord結合

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,26 +2,26 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods).where(order_foods: { id: nil })
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop
+    @shops = Shop.left_outer_joins(foods: [:order_foods]).where(order_foods: { id: nil }).distinct
   end
 
   def exercise3 
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address
+    @address = Address.joins(:orders).select("addresses.*,count(addresses.id) orders_count").group(:id).order("count(addresses.id) DESC").first
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer
+    @customer = Customer.joins(orders: [:foods]).select("customers.*,sum(foods.price) foods_price_sum").group(:id).order("sum(foods.price) DESC").first
   end
 end
